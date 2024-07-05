0717fb30c9ae5e5264bf9a0c96826828860c79d7
Thiago Torres <torrestube093@gmail.com>
2024-07-05 00:51:35 -0300

    Finalizando site

--- scripts.js
+++ scripts.js
@@ -17,3 +17,38 @@ document.addEventListener("DOMContentLoaded", function() {
         slider.querySelector(".slide").classList.add("active");
     }
 });
+
+// Perguntas sumirem
+
+function toggleTexto0() {
+    var t0 = document.getElementById("texto-escondido0");
+    if (t0.style.display === "none") {
+      t0.style.display = "block";
+    } else {
+      t0.style.display = "none";
+    }
+  }
+
+  function toggleTexto1() {
+    var t1 = document.getElementById("texto-escondido1");
+    if (t1.style.display === "none") {
+      t1.style.display = "block";
+    } else {
+      t1.style.display = "none";
+    }
+  }
+
+  function toggleTexto2() {
+    var t2 = document.getElementById("texto-escondido2");
+    if (t2.style.display === "none") {
+      t2.style.display = "block";
+    } else {
+      t2.style.display = "none";
+    }
+  }
+
+//   MENU 
+
+document.getElementById('toggleMenu').addEventListener('click', function() {
+    document.getElementById('sidebar').classList.toggle('active');
+});
